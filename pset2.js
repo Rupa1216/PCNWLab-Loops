2a2f08becfc0103a3af8f1967f6b5dea6905dee6
Srirupa Datta <srirupadatta@pursuit.org>
2018-10-21 11:51:02 -0400

    added solutions to problems 2 3 and 4

--- pset2.js
+++ pset2.js
@@ -5,6 +5,16 @@
     @desc - takes all engative numbers and makes positive
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
+// SOLUTION 1
+const removeNegatives = arr => {
+    const copyArr = [...arr];
+    for (i = 0; i < copyArr.length; i++) {
+        copyArr[i] = Math.abs(copyArr[i]);
+    }
+    return copyArr;
+}
+
+console.log(removeNegatives([-3, -2, 1, 2, 3, -4]));
 
 /*
     @func findVal
@@ -19,6 +29,25 @@
         findVal([1,2,3,4], 4); // 3
         findVal([1,2,3,4], 9); // -1
 */
+// SOLUTION 2
+const findVal = (arr, val) => {
+
+
+    for (i = 0; i < arr.length; i++) {
+
+        if (arr[i] === val) { // arr[i] refers to the element
+            return arr.indexOf(arr[i]);
+        }
+    }
+
+    return -1;
+}
+
+// wanna end when u end the A or find the val
+// as long as I haven't reached the end of the A, keep going
+console.log(findVal([1, 2, 3, 4], 1));
+console.log(findVal([1, 2, 3, 4], 4));
+console.log(findVal([1, 2, 3, 4], 9));
 
 /*
     @func removeOdds
@@ -31,6 +60,20 @@
         removeOdds([1,2,3,4]); // [2,4]
         removeOdds([1,"2",3,4]); // [4]
 */
+// SOLUTION 3
+const removeOdds = arr => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+    
+        if (arr[i] % 2 === 0 && typeof(arr[i]) === "number") {
+           newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+console.log(removeOdds([1, 2, 3, 4]));
+console.log(removeOdds([1, "2", 3, 4]));
 
 /*
     @func addSquares
@@ -40,6 +83,18 @@
     @example - 
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
+// SOLUTION 4
+const addSquares = arr => {
+const copyArr = [...arr];
+for (i = 0; i < arr.length; i++) {
+
+copyArr.push(arr[i] ** 2)
+}
+
+return copyArr;
+}
+
+console.log(addSquares([1,2,3,4]));
 
 /*
     @func doubleify
@@ -48,7 +103,18 @@
     @desc - take each value of array, add same value right after
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
+// SOLUTION 5
 
+const doubleify = arr => {
+    const copyArr =  [...arr];
+for (i = 0; i < arr.length; i++) {
+
+}
+}
+
+// 
+
+console.log(doubleify([1,2,3,4));
 
 /*
     @func findMax
@@ -57,3 +123,10 @@
     @desc - find largest number in array
     @example - findMax([1,2,3,99,4]); // 99 
 */
+// SOLUTION 6
+
+const findMax = arr => {
+for (i = 0; i < arr.length)
+}
+
+console.log(findMax([1,2,3,99,4]));
